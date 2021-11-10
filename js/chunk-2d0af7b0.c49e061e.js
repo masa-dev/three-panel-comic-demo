ffbc9d26162058e3a320a3de4834c77b377e466e
masa-dev <masa-dev@users.noreply.github.com>
2021-11-10 19:49:52 +0000

    deploy: 11c5e8fbf559f8a9b127bff1aabaca711deb8b6f

---
+++ js/chunk-2d0af7b0.c49e061e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0af7b0"],{"0f04":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{attrs:{id:"comic"}},[t("router-view")],1)},r=[],u=t("2877"),i={},a=Object(u["a"])(i,c,r,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0af7b0.c49e061e.js.map
---
+++ precache-manifest.d36e508a041240a4305dfd850d1d0f08.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760df620c7491daf6604",
+    "url": "/three-panel-comic-demo/css/app.3ab38a80.css"
+  },
+  {
+    "revision": "f4ea9596731f2bd59542",
+    "url": "/three-panel-comic-demo/css/chunk-57594562.a2b9ee6a.css"
+  },
+  {
+    "revision": "f05e9754dfd4663da028",
+    "url": "/three-panel-comic-demo/css/chunk-57a1b701.3a1f0193.css"
+  },
+  {
+    "revision": "8622ec5d8272c8ff400e",
+    "url": "/three-panel-comic-demo/css/chunk-e6c53c3e.add20aab.css"
+  },
+  {
+    "revision": "43068bc454121a4925b32ebe0b7c0567",
+    "url": "/three-panel-comic-demo/index.html"
+  },
+  {
+    "revision": "760df620c7491daf6604",
+    "url": "/three-panel-comic-demo/js/app.47fe5b11.js"
+  },
+  {
+    "revision": "31dbe58f679865496b19",
+    "url": "/three-panel-comic-demo/js/chunk-2d0ab43a.9ae9f78b.js"
+  },
+  {
+    "revision": "6b3660cd048784f7dd33",
+    "url": "/three-panel-comic-demo/js/chunk-2d0af7b0.c49e061e.js"
+  },
+  {
+    "revision": "216f41aaa4c088228b05",
+    "url": "/three-panel-comic-demo/js/chunk-43ed1f1d.b46e491b.js"
+  },
+  {
+    "revision": "f4ea9596731f2bd59542",
+    "url": "/three-panel-comic-demo/js/chunk-57594562.d0f085ad.js"
+  },
+  {
+    "revision": "f05e9754dfd4663da028",
+    "url": "/three-panel-comic-demo/js/chunk-57a1b701.5e393ca0.js"
+  },
+  {
+    "revision": "8622ec5d8272c8ff400e",
+    "url": "/three-panel-comic-demo/js/chunk-e6c53c3e.5483ed5d.js"
+  },
+  {
+    "revision": "daae1ae07a295e387c63",
+    "url": "/three-panel-comic-demo/js/chunk-vendors.a210eb94.js"
+  },
+  {
+    "revision": "5c55595b384d49855b9a5c15bd953d77",
+    "url": "/three-panel-comic-demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/three-panel-comic-demo/robots.txt"
+  }
+]);
