8dea164042c0dca7006173c59ee7388a9e953e1e
masa-dev <mssa.massan@gmail.com>
2021-11-11 04:47:29 +0900

    chore: モジュールの追加など
    firebase, flicking, axiosの追加
    firebaseのCORS設定のファイルの追加
    フォーマットの実行

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/three-panel-comic-demo/'
-    : '/'
-}
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/three-panel-comic-demo/" : "/",
+};
