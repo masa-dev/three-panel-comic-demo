b9e45d8e570873bb11aea48334eb9673f768dcfa
masa-dev <mssa.massan@gmail.com>
2021-11-20 01:03:58 +0900

    feat: ユーザー情報のページの追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,12 +2,23 @@
   <header>
     <div class="header-div">
       <div class="title-bar">
-        <h1>3コマ漫画アプリ</h1>
+        <router-link to="/">
+          <h1>3コマ漫画アプリ</h1>
+        </router-link>
       </div>
       <div class="user-bar">
-        <div class="user-link"></div>
-        <div class="login-link">
-          <router-link to="login">
+        <div class="user-link" v-if="isLogin">
+          <router-link to="/user">
+            <button class="header-user-photo">
+              <img :src="photoURL" alt="" />
+            </button>
+          </router-link>
+          <router-link to="/user">
+            <button class="header-user-name">{{ userName }}</button>
+          </router-link>
+        </div>
+        <div class="login-link" v-if="!isLogin">
+          <router-link to="/login">
             <button>ログイン</button>
           </router-link>
         </div>
@@ -16,7 +27,42 @@
   </header>
 </template>
 
+<script>
+import firebaseConfig from "../util/firebaseConfig";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+export default {
+  data() {
+    return {
+      isLogin: false,
+      userName: "",
+      photoURL: require("@/assets/no_user.png"),
+    };
+  },
+  mounted() {
+    firebase.initializeApp(firebaseConfig);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        if (user.providerData.length !== 0) {
+          this.isLogin = true;
+          this.userName = user.displayName;
+          this.photoURL = this.photoURL
+            ? user.photoURL
+            : require("@/assets/no_user.png");
+          return;
+        }
+      }
+      this.isLogin = false;
+    });
+  },
+};
+</script>
+
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap");
+
 $button-color: rgb(24, 120, 230);
 
 header {
@@ -32,17 +78,54 @@ header {
     margin-bottom: 10px;
     justify-content: space-between;
 
-    h1 {
-      font-size: 1.6em;
-      margin: {
-        top: 10px;
-        bottom: 10px;
+    .title-bar > a {
+      color: black;
+      text-decoration: none;
+      h1 {
+        font-size: 1.6em;
+        margin: {
+          top: 10px;
+          bottom: 10px;
+        }
       }
     }
 
     .user-bar {
       display: flex;
       align-items: center;
+
+      .user-link {
+        display: flex;
+        align-items: center;
+
+        button {
+          background-color: transparent;
+          border: none;
+          cursor: pointer;
+
+          &.header-user-photo {
+            width: 35px;
+            height: 35px;
+            padding: 0;
+
+            img {
+              width: 100%;
+              height: 100%;
+              object-fit: cover;
+              border-radius: 100%;
+              vertical-align: top;
+              overflow: hidden;
+            }
+          }
+
+          &.header-user-name {
+            padding: 0.5rem 0.5rem;
+            font-size: 1rem;
+            font-family: "Kosugi Maru", "Meiryo", serif;
+            font-weight: bold;
+          }
+        }
+      }
 
       .login-link {
         button {
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,5 +1,112 @@
 <template>
   <div id="user">
-    <h2>User Info</h2>
+    <h2>ユーザー情報</h2>
+    <p>ユーザー名： {{ userName }}</p>
+    <p>メールアドレス： {{ email }}</p>
+
+    <h3>ログアウト</h3>
+    <p>
+      <button @click="logout">ログアウトする</button>
+    </p>
+
+    <h3>アイコンの変更</h3>
+    <p>
+      <input
+        type="file"
+        id="user-icon-input"
+        @change="selectedImage"
+        accept="image/*"
+      />
+      <button @click="submitIcon">送信</button>
+    </p>
   </div>
 </template>
+
+<script>
+import firebaseConfig from "../util/firebaseConfig";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/storage";
+
+export default {
+  data() {
+    return {
+      userName: "",
+      email: "",
+      iconImage: null,
+    };
+  },
+  methods: {
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          alert("ログアウトしました");
+        });
+    },
+    selectedImage(e) {
+      this.iconImage = e.target.files[0];
+    },
+    submitIcon() {
+      if (!this.iconImage) {
+        return;
+      }
+
+      const user = firebase.auth().currentUser;
+      const storage = firebase.storage();
+      let fileType;
+
+      switch (this.iconImage.type) {
+        case "image/jpeg":
+          fileType = "jpg";
+          break;
+        case "image/png":
+          fileType = "png";
+          break;
+        default:
+          alert("pngまたはjpegファイル形式のみ可能です");
+          return;
+      }
+
+      const iconRef = storage
+        .ref("users")
+        .child(user.uid)
+        .child(`icon.${fileType}`);
+
+      iconRef.put(this.iconImage).then((snapshot) => {
+        snapshot.ref.getDownloadURL().then((url) => {
+          user.updateProfile({ photoURL: url });
+          alert("アップロードしました");
+        });
+      });
+    },
+  },
+  mounted() {
+    firebase.initializeApp(firebaseConfig);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      console.log(this.isLogin);
+      if (user) {
+        if (user.providerData.length !== 0) {
+          this.email = user.email;
+          this.userName = user.displayName;
+          return;
+        }
+      } else {
+        this.email = "";
+        this.userName = "";
+      }
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#user {
+  p {
+    margin-left: 2rem;
+  }
+}
+</style>
