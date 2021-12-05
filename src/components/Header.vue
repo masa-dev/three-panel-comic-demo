8ecbbdf536c76d2a79fbce72344da3a256bd6e8e
masa-dev <mssa.massan@gmail.com>
2021-12-06 02:05:24 +0900

    feat: ユーザー名の部分をクリックすると各リンクが表示される機能の追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,14 +8,25 @@
       </div>
       <div class="user-bar">
         <div class="user-link" v-if="isLogin">
-          <router-link to="/user">
-            <button class="header-user-photo">
-              <img :src="photoURL" alt="" />
-            </button>
-          </router-link>
-          <router-link to="/user">
-            <button class="header-user-name">{{ userName }}</button>
-          </router-link>
+          <button class="header-user-photo" @click="openModal">
+            <img :src="photoURL" alt="" />
+          </button>
+          <button class="header-user-name" @click="openModal">
+            {{ userName }}
+          </button>
+          <div class="modal-portal" v-if="modalSeen">
+            <div class="portal-backdrop" @click="closeModal"></div>
+            <div
+              class="portal-popover"
+              v-bind:style="modalPosition"
+              @click="closeModal"
+            >
+              <router-link to="/">トップページ</router-link>
+              <router-link to="/user">ユーザー設定</router-link>
+              <router-link to="/group">グループ設定</router-link>
+              <router-link to="/about">このサイトについて</router-link>
+            </div>
+          </div>
         </div>
         <div class="login-link" v-if="!isLogin">
           <router-link to="/login">
@@ -37,8 +48,33 @@ export default {
     return {
       isLogin: false,
       userName: "",
+      modalSeen: false,
+      modalPosition: {
+        top: "0px",
+        right: "0px",
+      },
       //photoURL: this.$store.state.user.photoURL,
     };
+  },
+  methods: {
+    openModal() {
+      this.modalSeen = true;
+      this.changeModalStyle();
+    },
+    closeModal() {
+      this.modalSeen = false;
+    },
+    changeModalStyle() {
+      if (!this.modalSeen) {
+        return;
+      }
+      let position = document
+        .getElementsByClassName("user-link")[0]
+        .getBoundingClientRect();
+
+      this.modalPosition.top = position.bottom + 5 + "px";
+      this.modalPosition.right = window.innerWidth - position.right + "px";
+    },
   },
   computed: {
     photoURL() {
@@ -46,6 +82,9 @@ export default {
     },
   },
   mounted() {
+    window.addEventListener("resize", this.changeModalStyle);
+    this.changeModalStyle();
+
     firebase.initializeApp(firebaseConfig);
 
     firebase.auth().onAuthStateChanged((user) => {
@@ -58,6 +97,14 @@ export default {
       }
       this.isLogin = false;
     });
+  },
+  beforeDestroy() {
+    window.addEventListener("resize", this.changeModalStyle);
+  },
+  watch: {
+    $route() {
+      this.modalSeen = false;
+    },
   },
 };
 </script>
@@ -125,6 +172,53 @@ header {
             font-size: 1rem;
             font-family: "Kosugi Maru", "Meiryo", serif;
             font-weight: bold;
+          }
+        }
+
+        // modal
+        .modal-portal {
+          .portal-popover {
+            position: absolute;
+            width: 200px;
+            border-radius: 5px;
+            background-color: rgb(245, 243, 235);
+            box-shadow: 0 3px 12px -1px #04253f40;
+            z-index: 100;
+
+            a {
+              padding: 0.7em 0.9em;
+              display: flex;
+              align-items: center;
+              flex-wrap: wrap;
+              // width: 100%;
+              text-align: left;
+              color: black;
+              outline: 0;
+              line-height: 1.5;
+              font-size: 15px;
+              text-decoration: none;
+              border-top: solid rgb(182, 182, 182) 1px;
+              transition: 0.2s;
+
+              &:first-child {
+                border: none;
+              }
+
+              &:hover {
+                background-color: rgb(240, 237, 226);
+              }
+            }
+          }
+
+          .portal-backdrop {
+            position: fixed;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background: transparent;
+            inset: 0px;
+            cursor: auto;
+            z-index: 99px;
           }
         }
       }
