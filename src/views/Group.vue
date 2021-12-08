8448a9ff199745055e572bbfca5a901f27dcfdd8
masa-dev <mssa.massan@gmail.com>
2021-12-08 23:20:00 +0900

    feat: グループ機能の進行状況を追加

--- src/views/Group.vue
+++ src/views/Group.vue
@@ -1,6 +1,55 @@
 <template>
   <div id="group">
-    <h2>所属グループ</h2>
-    <div></div>
+    <div v-if="group.isJoin">
+      <h2>所属グループ</h2>
+    </div>
+    <div v-if="!group.isJoin">
+      <h2>グループ</h2>
+      <p>現在、グループに所属していません。</p>
+    </div>
   </div>
 </template>
+
+<script>
+import firebaseConfig from "@/util/firebaseConfig.js";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+
+export default {
+  data() {
+    return {
+      group: {
+        isJoin: false,
+        name: "",
+      },
+    };
+  },
+  mounted() {
+    firebase.initializeApp(firebaseConfig);
+
+    let unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        let user = firebase.auth().currentUser;
+        let userGroupRef = firebase.database().ref(`users/${user.uid}/group`);
+
+        userGroupRef.on("value", (snapshot) => {
+          let groupId = snapshot.val();
+
+          if (groupId) {
+            console.log(groupId);
+            this.group.isJoin = true;
+          }
+
+          // グループに所属していない場合
+          else {
+            this.group.isJoin = false;
+          }
+        });
+        console.log(user, userGroupRef, user.uid);
+      }
+
+      unsubscribe();
+    });
+  },
+};
+</script>
