36c3d95835b1b86c715de818c39bced2ff8c161f
masa-dev <mssa.massan@gmail.com>
2021-12-13 14:55:23 +0900

    feat: ユーザー情報のページのアップデート
    ユーザーアイコンの画像の表示
    ボタンやインプットのスタイルの更新
    ユーザー名を変更する機能の追加

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,23 +1,47 @@
 <template>
   <div id="user">
     <h2>ユーザー情報</h2>
-    <p>ユーザー名： {{ userName }}</p>
-    <p>メールアドレス： {{ email }}</p>
+
+    <div class="user-info-wrap">
+      <label for="user-name-input">ユーザー情報</label>
+      <input type="text" id="user-name-input" :value="userName" />
+    </div>
+    <div class="user-info-wrap">
+      <label for="user-mail-input">メールアドレス(変更不可)</label>
+      <input type="text" id="user-mail-input" :value="email" readonly />
+    </div>
+    <div class="user-info-wrap">
+      <button @click="updateProfile">変更を更新する</button>
+    </div>
+
+    <h3>アイコンの変更</h3>
+    <div class="user-info-wrap">
+      <label for="user-icon-input">
+        <input
+          type="file"
+          id="user-icon-input"
+          style="display: none"
+          @change="selectedImage"
+          accept="image/*"
+        />
+        <div class="user-icon-wrap">
+          <img
+            :src="photoURL"
+            alt=""
+            width="110"
+            height="110"
+            id="user-icon-input-img"
+          />
+        </div>
+      </label>
+    </div>
+    <div class="user-info-wrap">
+      <button @click="submitIcon">送信</button>
+    </div>
 
     <h3>ログアウト</h3>
     <p>
-      <button @click="logout">ログアウトする</button>
-    </p>
-
-    <h3>アイコンの変更</h3>
-    <p>
-      <input
-        type="file"
-        id="user-icon-input"
-        @change="selectedImage"
-        accept="image/*"
-      />
-      <button @click="submitIcon">送信</button>
+      <button class="logout" @click="logout">ログアウトする</button>
     </p>
   </div>
 </template>
@@ -27,16 +51,35 @@ import firebaseConfig from "../util/firebaseConfig";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/storage";
+import "firebase/compat/database";
 
 export default {
   data() {
     return {
-      userName: "",
       email: "",
       iconImage: null,
     };
   },
   methods: {
+    updateProfile() {
+      let userName = document.getElementById("user-name-input").value;
+
+      let unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          console.log(user);
+          const database = firebase.database();
+          const userRef = database
+            .ref()
+            .child("users")
+            .child(user.uid)
+            .child("username");
+          userRef.set(userName);
+
+          this.$store.commit("updateUserName", userName);
+        }
+        unsubscribe();
+      });
+    },
     logout() {
       if (confirm("ログアウトしますか？")) {
         firebase
@@ -48,7 +91,12 @@ export default {
       }
     },
     selectedImage(e) {
-      this.iconImage = e.target.files[0];
+      if (e.target.files[0]) {
+        this.iconImage = e.target.files[0];
+
+        let img = document.getElementById("user-icon-input-img");
+        img.src = URL.createObjectURL(this.iconImage);
+      }
     },
     submitIcon() {
       if (!this.iconImage) {
@@ -85,6 +133,14 @@ export default {
       });
     },
   },
+  computed: {
+    photoURL() {
+      return this.$store.state.user.photoURL;
+    },
+    userName() {
+      return this.$store.state.user.userName;
+    },
+  },
   mounted() {
     firebase.initializeApp(firebaseConfig);
 
@@ -92,12 +148,10 @@ export default {
       if (user) {
         if (user.providerData.length !== 0) {
           this.email = user.email;
-          this.userName = user.displayName;
           return;
         }
       } else {
         this.email = "";
-        this.userName = "";
       }
     });
   },
@@ -105,9 +159,90 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$inputColor: rgb(60, 161, 255);
+$inputShadowColor: rgba(60, 161, 255, 0.3);
+
 #user {
   p {
     margin-left: 2rem;
+  }
+
+  .user-icon-wrap {
+    display: inline-block;
+    width: 110px;
+    height: 110px;
+    cursor: pointer;
+
+    img {
+      object-fit: cover;
+      border-radius: 100%;
+      vertical-align: top;
+      overflow: hidden;
+      box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+    }
+  }
+
+  button {
+    color: white;
+    background-color: $inputColor;
+    font-weight: bold;
+    font-size: 0.9rem;
+    padding: 0.4rem 0.75rem;
+    border: 1px solid $inputColor;
+    border-radius: 5px;
+    transition: 0.3s;
+    cursor: pointer;
+
+    &:hover,
+    &:focus {
+      outline: none;
+      border-color: $inputColor;
+      box-shadow: 0 0 0 2px $inputShadowColor;
+    }
+
+    &.logout {
+      color: $inputColor;
+      background-color: white;
+
+      &:hover,
+      &:focus {
+        color: white;
+        background-color: $inputColor;
+      }
+    }
+  }
+
+  .user-info-wrap {
+    margin: {
+      left: 2em;
+      top: 1em;
+      bottom: 1em;
+    }
+
+    input[type="text"] {
+      display: block;
+      width: 400px;
+      font-size: 0.85em;
+      margin: {
+        top: 0.5em;
+        bottom: 0.5em;
+      }
+      padding: {
+        top: 0.8em;
+        left: 1em;
+        bottom: 0.7em;
+      }
+      border: 1px solid rgb(182, 182, 182);
+      border-radius: 5px;
+      transition: 0.3s;
+
+      &:hover,
+      &:focus {
+        outline: none;
+        border-color: $inputColor;
+        box-shadow: 0 0 0 2px $inputShadowColor;
+      }
+    }
   }
 }
 </style>
