a52aaaf43c0f2d750da35d97cd2dba4dca240b40
masa-dev <mssa.massan@gmail.com>
2021-12-13 15:07:22 +0900

    fix: pwaで更新されない問題の修正

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production" ? "/three-panel-comic-demo/" : "/",
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
 };
