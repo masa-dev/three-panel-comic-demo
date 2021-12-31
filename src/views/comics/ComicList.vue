df615cc6ac52da082a23f159f872f6fb4153cb13
masa-dev <mssa.massan@gmail.com>
2021-12-31 17:27:36 +0900

    feat: 漫画の一覧を自動で取得する機能を追加

--- src/views/comics/ComicList.vue
+++ src/views/comics/ComicList.vue
@@ -2,14 +2,9 @@
   <div class="comic-list">
     <h2>漫画一覧</h2>
     <ul class="comic-link-list">
-      <li>
-        <router-link to="comics/1"
-          ><div class="comic-link">泣きっ面に蜂</div>
-        </router-link>
-      </li>
-      <li>
-        <router-link to="comics/1"
-          ><div class="comic-link">泣きっ面に蜂</div>
+      <li v-for="(link, index) in links" :key="index">
+        <router-link :to="link"
+          ><div class="comic-link">{{ titles[index] }}</div>
         </router-link>
       </li>
     </ul>
@@ -17,7 +12,38 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      originalPath: "https://buturi.heteml.net/student/2021/toda/comics/",
+      comicLength: 0,
+      links: [],
+      titles: [],
+    };
+  },
+  async mounted() {
+    await fetch(`${this.originalPath}countComic.php`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.comicLength = data.count;
+      })
+      .catch((e) => console.error(e));
+
+    for (let i = 1; i <= this.comicLength; i++) {
+      this.links.push(`comics/${i}`);
+      await fetch(`${this.originalPath}getData.php?num=${i}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.status === false) {
+            console.error("fetch error: getData file throw error");
+          } else {
+            this.titles.push(data.title);
+          }
+        })
+        .catch((e) => console.error(e));
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -32,10 +58,15 @@ export default {};
   li {
     width: 49%;
     box-sizing: border-box;
-    border: 1px solid gray;
+    background-color: rgb(253, 229, 198);
+    border: 1px solid rgb(218, 184, 140);
     border-radius: 10px;
     &:nth-child(2n-1) {
       margin-right: 2%;
+    }
+
+    &:hover {
+      background-color: rgb(241, 215, 184);
     }
   }
 
