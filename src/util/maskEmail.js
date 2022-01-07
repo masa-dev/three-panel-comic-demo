484b0a64867b5fba8ae7e32e77cc5fc0fe88dfec
masa-dev <mssa.massan@gmail.com>
2022-01-08 03:23:32 +0900

    feat: 管理者が見れるメンバーリストを更新
    ユーザー名、ID、メールを表示する
    Eメールはマスキングした上で表示する
    各項目でソートをできる機能の追加

---
+++ src/util/maskEmail.js
@@ -0,0 +1,29 @@
+/**
+ * メールアドレスをマスキングする関数
+ * @param {String} email メールアドレスの文字列
+ * @returns {String} マスキング後のメールアドレス
+ */
+function maskEmail(email) {
+  if (email && typeof email === "string") {
+    return email.replace(/^\w+([-+.]\w+)*@/, (match) => {
+      let masked = "";
+      let startString = "";
+      let endString = "";
+      const length = match.length;
+      // メールの長さ次第で表示領域を変える
+      const displayLength = length > 8 ? 2 : 1;
+
+      // 表示する最初と最後の文字列を抽出
+      startString = match.slice(0, displayLength);
+      endString = match.slice(length - displayLength - 1, length);
+
+      // 表示しない部分は * でマスキングする
+      masked = startString + "*".repeat(length - displayLength * 2) + endString;
+      return masked;
+    });
+  } else {
+    return `${email}is not a string`;
+  }
+}
+
+export default maskEmail;
