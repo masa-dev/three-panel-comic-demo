9658911ce2e4ecb409f1bdea31464af25730c4a2
masa-dev <mssa.massan@gmail.com>
2022-01-18 19:23:54 +0900

    chore: "test/util/"ディレクトリ内のテストファイルを実行できるように設定を変更

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
+  testMatch: [
+    "**/__tests__/*.{j,t}s?(x)",
+    "**/tests/unit/**/*.spec.{j,t}s?(x)",
+    "**/tests/util/**/*.spec.{j,t}s?(x)",
+  ],
 };
